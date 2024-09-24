8c0bd2e5b107dd0d9d545387d9f91961374e942d
Benncio09 <lorebencini13@outlook.com>
2024-09-24 11:21:22 +0200

    es 6 finito e funzionante

---
+++ esercizio6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        $N=20;
+        echo "<ul>";
+        for($i=1; $i<$N;$i++){
+            if($N%$i==0){
+            echo "<li>$i</li>";
+            }
+        }
+        echo "</ul>";
+    ?>
+    
+</body>
+</html>
